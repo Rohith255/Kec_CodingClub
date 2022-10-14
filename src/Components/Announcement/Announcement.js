0aad8dd0185d4967a68eedf4027a7e1fe3129d09
techguyash <techguyashash@gmail.com>
2022-10-14 11:56:04 +0530

    Layout commit

---
+++ src/Components/Announcement/Announcement.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Announcement.css";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import picBg from "../../assets/announcement.svg";
+
+const Announcement = () => {
+  return (
+    <div className="announcement">
+      <div className="announcement-title">Announcements</div>
+
+      <div className="announcement-body">
+        <div className="announcement-banner">
+          <img src={picBg} className="announce-pic" />
+        </div>
+        <div className="announce-carousal">
+          <Carousel dynamicHeight={true} infiniteLoop={true} autoPlay={true}>
+            <div className="announcement-post">
+              <h1 className="announcement-heading">Enthusia 2k21</h1>
+              <h2 className="announcement-desc">
+                Conducted by KEC cultural club, On 26-05-2020 Lorem ipsum dolor
+                sit amet consectetur, adipisicing elit. Veniam expedita quidem
+                odio modi repellendus suscipit tenetur obcaecati. Quae excepturi
+                eius cupiditate hic cumque, architecto, praesentium natus,
+                quaerat magni doloribus explicabo.
+              </h2>
+            </div>
+            <div className="announcement-post">
+              <h1 className="announcement-heading">Enthusia 2k21</h1>
+              <h2 className="announcement-desc">
+                Conducted by KEC cultural club, On 26-05-2020 Lorem ipsum dolor
+                sit amet consectetur, adipisicing elit. Veniam expedita quidem
+                odio modi repellendus suscipit tenetur obcaecati. Quae excepturi
+                eius cupiditate hic cumque, architecto, praesentium natus,
+                quaerat magni doloribus explicabo.
+              </h2>
+            </div>
+          </Carousel>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Announcement;
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Banner.css";
+import Typewriter from "typewriter-effect";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+const Banner = () => {
+  return (
+    <div className="banner">
+      <div className="banner_nav">
+        <div className="icon">logo</div>
+        <div className="banner_menu">
+          <div className="banner_menu_container">
+            <div className="menu-item">Home</div>
+            <div className="menu-item">About</div>
+            <div className="menu-item login">Sign In</div>
+            <div className="menu-item">Sign Up</div>
+          </div>
+        </div>
+      </div>
+      <div className="banner_content">
+        <div className="heading"> Welcome to</div>
+        <div className="heading2">
+          <Typewriter
+            options={{
+              strings: ["Coding Club", "MCA"],
+              autoStart: true,
+              loop: true,
+            }}
+          />
+        </div>
+
+        <div className="banner_button">
+          Let's Get Started
+          <div className="swipe_down">
+            <span class="material-symbols-outlined down">expand_more</span>
+            <span class="material-symbols-outlined ">expand_more</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
